39d31af133b0c41e6ab735bd722939307a672e68
Alex <axsk@users.noreply.github.com>
2023-03-06 00:08:44 +0100

    remove html fixed position (broke scroll)

--- src/App.svelte
+++ src/App.svelte
@@ -12,14 +12,15 @@
 
 <style>
 :global(html) {
-  position: fixed;
   height: 100%;
+  overflow: hidden;
 }
 
 :global(body) {
   width: 100vw;
   height: 100vh;
   overflow-y: scroll;
+  overflow-x: hidden;
   -webkit-overflow-scrolling: touch;
 }
 </style>
